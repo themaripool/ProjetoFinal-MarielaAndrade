9175265a6ac9e7b2c710f0e3d66f95cc29203386
themaripool <marielama@outlook.com.br>
2021-05-23 16:40:37 -0300

    [FIX] Selection view buttons layout

--- lib/Screens/Dashboard.dart
+++ lib/Screens/Dashboard.dart
@@ -41,43 +41,69 @@ class _DashboardState extends State<Dashboard> {
       container = ListViewPatients();
     }
 
-    //   Column(children: [
-    //     new Text('app'),
-    //     new Expanded(
-    //         child: new Center(
-    //             child: new Container(
-    //                 height: [EXPECTED_GRID_HEIGHT],
-    //                 child: GridView.count(
-    //                   padding: const EdgeInsets.all(20.0),
-    //                   crossAxisSpacing: 10.0,
-    //                   mainAxisSpacing: 10.0,
-    //                   crossAxisCount: 4,
-    //                   children: [GRID_ELEMENTS],
-    //                 )))),
-    //     new Text('app'),
-    //   ]),
-    // }
-
-    //  ElevatedButton(
-    //         onPressed: () {
-    //           setState(() {
-    //             status = !status;
-    //             print(status);
-    //           });
-    //         },
-    //         child: Text(' Disable / Enable')),
-
     return Scaffold(
         body: Center(
       child: Column(children: [
-        ElevatedButton(
-            onPressed: () {
-              setState(() {
-                status = !status;
-                print(status);
-              });
-            },
-            child: Text(' Disable / Enable')),
+        Padding(
+          padding: const EdgeInsets.only(left: 8, right: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                    width: 1.5,
+                    color: Colors.black,
+                  )),
+                  onPressed: () {
+                    print('Received click');
+                  },
+                  child: const Text(
+                    'Alarmes',
+                    style: TextStyle(color: Colors.black),
+                  )),
+              Spacer(),
+              OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                      backgroundColor: status == true
+                          ? Colors.grey[850]
+                          : Colors.transparent,
+                      side: BorderSide(
+                        width: 1.5,
+                        color: Colors.black,
+                      )),
+                  onPressed: () {
+                    setState(() {
+                      status = true;
+                      print(status);
+                    });
+                  },
+                  child: Icon(
+                    Icons.grid_on,
+                    color: status == false ? Colors.black : Colors.white,
+                  )),
+              OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                      backgroundColor: status == false
+                          ? Colors.grey[850]
+                          : Colors.transparent,
+                      side: BorderSide(
+                        width: 1.5,
+                        color: Colors.black,
+                      )),
+                  onPressed: () {
+                    setState(() {
+                      status = false;
+                      print(status);
+                    });
+                  },
+                  child: Icon(
+                    Icons.list,
+                    color: status == false ? Colors.white : Colors.black,
+                  )),
+            ],
+          ),
+        ),
         Expanded(child: Center(child: container)),
       ]),
     ));
