bea3e4a7a02b0139af3602c0a9b2b9d90283cad0
Mari Andrade <mariela.andrade@ingresso.com>
2021-06-23 12:28:58 -0300

    [CHANGE] Patient Screen

--- lib/Screens/PatientData.dart
+++ lib/Screens/PatientData.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:intl/intl.dart';
 
 class _News2Data {
   _News2Data(this.pulse, this.time, this.temp);
@@ -17,42 +18,52 @@ class PatientData extends StatefulWidget {
 class _PatientDataState extends State<PatientData> {
   List<_News2Data> data = [
     _News2Data(100.0, "17:55", 35.6),
-    _News2Data(97.0, "18:05", 34.9 ),
-    _News2Data(104.0, "18:15", 36.5 ),
-    _News2Data(109.0, "18:28", 37.3 ),
-    _News2Data(99.0, "18:55", 36.9 )
+    _News2Data(97.0, "18:05", 34.9),
+    _News2Data(104.0, "18:15", 36.5),
+    _News2Data(109.0, "18:28", 37.3),
+    _News2Data(99.0, "18:55", 36.9)
   ];
   @override
   Widget build(BuildContext context) {
+    DateTime date = DateTime.now();
+    final day = date.day;
+    final month = date.month;
+    final dayOfWeek = DateFormat('EEEE').format(date);
+
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          
-          SfCartesianChart(
-            tooltipBehavior: TooltipBehavior(enable: true),
-            primaryXAxis: CategoryAxis(), 
-            series: <CartesianSeries>[
-            // Renders line chart
-            SplineSeries<_News2Data, String>(
-                dataSource: data,
-                name: "Pulso",
-                enableTooltip: true, 
-                xValueMapper: (_News2Data data, _) => data.time,
-                yValueMapper: (_News2Data data, _) => data.pulse,
-                markerSettings: MarkerSettings(isVisible: true)
+          Padding(
+            padding: const EdgeInsets.only(top: 16, bottom: 32, left: 8),
+            child: Text(
+              "$dayOfWeek, $day / $month",
+              style: TextStyle(fontSize: 30, fontStyle: FontStyle.italic),
             ),
+          ),
+          SfCartesianChart(
+              tooltipBehavior: TooltipBehavior(enable: true),
+              primaryXAxis: CategoryAxis(),
+              series: <CartesianSeries>[
+                // Renders line chart
+                SplineSeries<_News2Data, String>(
+                    dataSource: data,
+                    name: "Pulso",
+                    enableTooltip: true,
+                    xValueMapper: (_News2Data data, _) => data.time,
+                    yValueMapper: (_News2Data data, _) => data.pulse,
+                    markerSettings: MarkerSettings(isVisible: true)),
 
-            SplineSeries<_News2Data, String>(
-              color: Colors.red,
-              dataSource: data,
-              name: "Temperatura",
-              enableTooltip: true, 
-              xValueMapper: (_News2Data data, _) => data.time,
-              yValueMapper: (_News2Data data, _) => data.temp,
-              markerSettings: MarkerSettings(isVisible: true)
-            )
-
-          ]),
+                SplineSeries<_News2Data, String>(
+                    color: Colors.red,
+                    dataSource: data,
+                    name: "Temperatura",
+                    enableTooltip: true,
+                    xValueMapper: (_News2Data data, _) => data.time,
+                    yValueMapper: (_News2Data data, _) => data.temp,
+                    markerSettings: MarkerSettings(isVisible: true))
+              ]),
         ],
       ),
     );
