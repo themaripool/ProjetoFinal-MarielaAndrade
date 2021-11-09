34baad8298d23b8398555058a33ccfeee8755505
themaripool <marielama@outlook.com.br>
2021-11-09 09:32:13 -0300

    [FIX] Tela de sintomas funcionando no postgress

--- lib/Data/DAO/symptomsDAO.dart
+++ lib/Data/DAO/symptomsDAO.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:postgres/postgres.dart';
+import 'package:projeto_final_1/Data/Data.dart';
+import 'package:projeto_final_1/Models/Models.dart';
 
 class SymptomsDao {
   PostgreSQLConnection _symptomsRef;
@@ -9,12 +13,45 @@ class SymptomsDao {
 
   /*  Query getMessageQuery() {
     return _symptomsRef;
-  }
+  }*/
 
-  Query getAlarmsByUser(String user) {
+  Future<List<Symptom>> getAlarmsByUser(String user) async {
     print("------- QUERY BED NUMBER $user -------");
-    return _symptomsRef.host;//.orderByChild("user").equalTo(user);
-  } */
+
+    //SELECT *
+    //FROM "SmartAlarmMobile_symptoms"
+    //WHERE userlogged = 'teste'
+
+    List<Symptom> res = List<Symptom>();
+
+    print("[BD TESTE]: entrou");
+
+    _symptomsRef = PostgreSQLConnection(
+      '10.0.2.2',
+      5435,
+      'postgres',
+      username: 'postgres',
+      password: 'secret',
+    );
+    await _symptomsRef.open();
+
+    print("[BD TESTE]: ${_symptomsRef.isClosed}");
+
+    var result = await _symptomsRef.query(
+        '''select * from "SmartAlarmMobile_symptoms" WHERE userlogged = '$user' ''');
+
+    var aux;
+
+    result.forEach((element) => {
+       aux = Symptom(element[3], element[9], element[8], 
+                    element[5], element[1], element[6],element[4], 
+                    element[7], element[0], element[2], element[10]),
+       res.add(aux)
+    });  
+
+    print("[BD TESTE]: RES = $result res = $res");
+    return res;
+  }
 
   void initPostgres() async {
     _symptomsRef = PostgreSQLConnection(
@@ -41,8 +78,7 @@ class SymptomsDao {
       String pain,
       String tiredness,
       String userlogged) async {
-      
-      _symptomsRef = PostgreSQLConnection(
+    _symptomsRef = PostgreSQLConnection(
       '10.0.2.2',
       5435,
       'postgres',
--- lib/Screens/InpatientUseCase/Pages/InpatientSymptomsHistory.dart
+++ lib/Screens/InpatientUseCase/Pages/InpatientSymptomsHistory.dart
@@ -2,11 +2,9 @@
 - Mostra histórico de sintomas de acordo com o so
 */
 
-import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:projeto_final_1/Data/Data.dart';
-import 'package:projeto_final_1/Models/Models.dart';
 
 class InpatientSymptomsHistory extends StatelessWidget {
   final String bedNumber;
@@ -19,50 +17,77 @@ class InpatientSymptomsHistory extends StatelessWidget {
     final _platform = Theme.of(context).platform;
     if (_platform == TargetPlatform.iOS) {
       return Scaffold(
-     /*  resizeToAvoidBottomInset: false,
-      appBar: CupertinoNavigationBar(middle: Text("Histórico"),),
-      body: FirebaseAnimatedList(
-        defaultChild:Center(child: CircularProgressIndicator()),
-        query: SymptomsDao().getAlarmsByUser(Provider.of<BedProvider>(contextNavigation, listen: false).currentUserName),
-        itemBuilder: (context, snapshot, animation, index) {
-          final json = snapshot.value as Map<dynamic, dynamic>;
-          final symptom = Symptom.fromJson(json);
-          return SymptomsComponentList(
-              symptom.headache,
-              symptom.nausea,
-              symptom.tiredness,
-              symptom.diarrea,
-              symptom.pain,
-              symptom.others,
-              symptom.hourAndMinute,
-              symptom.formattedDate
-              );
-        },
-      ), */
-    );
+        resizeToAvoidBottomInset: false,
+        appBar: CupertinoNavigationBar(
+          middle: Text("Histórico"),
+        ),
+        body: FutureBuilder<List>(
+          future: SymptomsDao().getAlarmsByUser(
+              Provider.of<BedProvider>(contextNavigation, listen: false)
+                  .currentUserName),
+          initialData: List(),
+          builder: (context, snapshot) {
+            return snapshot.hasData
+                ? ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (_, int position) {
+                      final symptom = snapshot.data[position];
+                      return SymptomsComponentList(
+                        symptom.headache,
+                        symptom.nausea,
+                        symptom.tiredness,
+                        symptom.diarrea,
+                        symptom.pain,
+                        symptom.others,
+                        symptom.hourAndMinute,
+                        symptom.formattedDate,
+                        symptom.ox,
+                        symptom.conscience,
+                      );
+                    },
+                  )
+                : Center(child: CircularProgressIndicator());
+          },
+        ),
+      );
     } else {
       return Scaffold(
-     /*  resizeToAvoidBottomInset: false,
-      appBar: AppBar(title: Text("Histórico"), backgroundColor: Colors.grey[700],),
-      body: FirebaseAnimatedList(
-        defaultChild:Center(child: CircularProgressIndicator()),
-        query: SymptomsDao().getAlarmsByUser(Provider.of<BedProvider>(contextNavigation, listen: false).currentUserName),
-        itemBuilder: (context, snapshot, animation, index) {
-          final json = snapshot.value as Map<dynamic, dynamic>;
-          final symptom = Symptom.fromJson(json);
-          return SymptomsComponentList(
-              symptom.headache,
-              symptom.nausea,
-              symptom.tiredness,
-              symptom.diarrea,
-              symptom.pain,
-              symptom.others,
-              symptom.hourAndMinute,
-              symptom.formattedDate);
-        },
-      ), */
-    );
-
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text("Histórico"),
+          backgroundColor: Colors.grey[700],
+        ),
+        body: FutureBuilder<List>(
+          future: SymptomsDao().getAlarmsByUser(
+              Provider.of<BedProvider>(contextNavigation, listen: false)
+                  .currentUserName),
+          initialData: List(),
+          builder: (context, snapshot) {
+            return snapshot.hasData
+                ? ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (_, int position) {
+                      final symptom = snapshot.data[position];
+                      return SymptomsComponentList(
+                        symptom.headache,
+                        symptom.nausea,
+                        symptom.tiredness,
+                        symptom.diarrea,
+                        symptom.pain,
+                        symptom.others,
+                        symptom.hourAndMinute,
+                        symptom.formattedDate,
+                        symptom.ox,
+                        symptom.conscience,
+                      );
+                    },
+                  )
+                : Center(
+                    child: CircularProgressIndicator(),
+                  );
+          },
+        ),
+      );
     }
   }
 }
@@ -76,13 +101,24 @@ class SymptomsComponentList extends StatelessWidget {
   final String others;
   final String hourAndMinute;
   final String formattedDate;
-  SymptomsComponentList(this.headache, this.nausea, this.tiredness,
-      this.diarrea, this.pain, this.others, this.hourAndMinute, this.formattedDate);
+  final String ox;
+  final String conscience;
+  SymptomsComponentList(
+      this.headache,
+      this.nausea,
+      this.tiredness,
+      this.diarrea,
+      this.pain,
+      this.others,
+      this.hourAndMinute,
+      this.formattedDate,
+      this.ox,
+      this.conscience);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 190.0,
+      height: 230.0,
       child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -90,21 +126,36 @@ class SymptomsComponentList extends StatelessWidget {
             children: [
               Padding(
                 padding: const EdgeInsets.only(bottom: 16.0),
-                child: Row(
-                  children: [
+                child: Row(children: [
                   Text(formattedDate,
-                      style: TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold)),
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold)),
                   Spacer(),
                   Text(hourAndMinute,
-                      style: TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold)),
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold)),
                 ]),
               ),
-              Text("Dor de Cabeça: $headache", style: TextStyle(fontSize: 16, color: Colors.black)),
-              Text("Nausea: $nausea", style: TextStyle(fontSize: 16, color: Colors.black)),
-              Text("Cansaço $tiredness", style: TextStyle(fontSize: 16, color: Colors.black)),
-              Text("Diarreia: $diarrea", style: TextStyle(fontSize: 16, color: Colors.black)),
-              Text("Dor: $pain", style: TextStyle(fontSize: 16, color: Colors.black)),
-              Text("Outros: $others", style: TextStyle(fontSize: 16, color: Colors.black)),
+              Text("Dor de Cabeça: $headache",
+                  style: TextStyle(fontSize: 16, color: Colors.black)),
+              Text("Nausea: $nausea",
+                  style: TextStyle(fontSize: 16, color: Colors.black)),
+              Text("Cansaço $tiredness",
+                  style: TextStyle(fontSize: 16, color: Colors.black)),
+              Text("Diarreia: $diarrea",
+                  style: TextStyle(fontSize: 16, color: Colors.black)),
+              Text("Dor: $pain",
+                  style: TextStyle(fontSize: 16, color: Colors.black)),
+              Text("Outros: $others",
+                  style: TextStyle(fontSize: 16, color: Colors.black)),
+              Text("Suplemento de ox?: $ox",
+                  style: TextStyle(fontSize: 16, color: Colors.black)),
+              Text("Consciência: $conscience",
+                  style: TextStyle(fontSize: 16, color: Colors.black)),
             ],
           )),
     );
